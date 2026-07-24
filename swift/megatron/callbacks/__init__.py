8d1ec808f3704ea09a2b043a89e3894582429b0a
umdetree <umdetree@github.com>
2026-07-24 14:23:48 +0800

    refactor: delete swift

--- swift/megatron/callbacks/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright (c) ModelScope Contributors. All rights reserved.
-from .base import MegatronCallback
-from .mapping import megatron_callbacks_map
--- swift/model/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import (baai, baichuan, baidu, bert, codefuse, deepseek, gemma, glm, internlm, latent_cot, llama, llava, llm,
-               mamba, microsoft, minicpm, minimax, mistral, mllm, moonshot, mplug, openbuddy, qwen, seed, skywork,
-               stepfun, telechat, tencent, valley, yi)
